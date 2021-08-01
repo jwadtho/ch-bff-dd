c6c3aaa73659cfc02bc8ec5dc81592418855b5f7
Jannarong Wadthong <toojannarong@gmail.com>
2021-08-02 01:12:04 +0700

    add log to jaeger properties

--- src/main/kotlin/com/tootest/ch/bff/configuration/JaegerConfiguration.kt
+++ src/main/kotlin/com/tootest/ch/bff/configuration/JaegerConfiguration.kt
@@ -2,6 +2,7 @@ package com.tootest.ch.bff.configuration
 
 import io.jaegertracing.Configuration.*
 import io.opentracing.Tracer
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,9 +11,11 @@ import org.springframework.context.annotation.Configuration
 @ConditionalOnProperty(name=["jaeger.enabled"], havingValue="true")
 @Configuration(proxyBeanMethods = false)
 class JaegerConfiguration(private val jaegerProperties: JaegerProperties) {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @Bean
     fun tracer(): Tracer {
+        logger.info("[JaegerProperties] {}", jaegerProperties)
         val config = io.jaegertracing.Configuration(jaegerProperties.serviceName)
         val sender = SenderConfiguration()
         sender.withEndpoint(jaegerProperties.endpoint)
