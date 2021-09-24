2e18b67d25af30bb3221c2b6222091a461d6527f
Jannarong Wadthong <toojannarong@gmail.com>
2021-09-25 05:55:14 +0700

    use auto inject sidecar

--- docker-build-run.sh
+++ docker-build-run.sh
@@ -4,5 +4,5 @@ imageBuild=ch-bff:0.1
 docker build -t $imageBuild .
 
 docker run -p 127.0.0.1:9300:9300/tcp \
-        -e SPRING_PROFILES_ACTIVE=k8s  \
+        -e SPRING_PROFILES_ACTIVE=dd  \
         $imageBuild
